0ef5593ce3adcaca33c610ba02b468db6b6d9776
IlyasseMokhtari <ilyassemokhtari@gmail.com>
2018-06-26 16:19:42 +0200

    gg

---
+++ hmlpython.py
@@ -0,0 +1,25 @@
+print "Content-Type: text/HTML"
+print 
+
+
+
+print"""
+<html>
+   <head>
+       <meta charset="utf-8">
+       <title>Hello Word</title>
+
+
+
+</head>
+
+
+<body>
+
+<h1>Hello Word !!</h1>
+
+
+</body>
+
+</html>
+"""
